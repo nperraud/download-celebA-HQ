13bd4287f970f6890cb7830548b57c10f03d6fa1
Nathanael Perraudin <nperraud@users.noreply.github.com>
2018-04-23 16:08:03 +0200

    first working version of download_celeba?

--- download_celebA.py
+++ download_celebA.py
@@ -8,7 +8,6 @@ if sys.version_info[0] > 2:
     from urllib.request import urlretrieve
 else:
     from urllib import urlretrieve
-import numpy as np
 from subprocess import Popen
 
 
@@ -29,7 +28,7 @@ def checksum(filename, method='sha1'):
     return None
 
 
-def download(url, target_dir='./data/', filename=None):
+def download(url, target_dir, filename=None):
     require_dir(target_dir)
     if filename is None:
         filename = url_filename(url)
@@ -60,23 +59,34 @@ def archive_extract(filepath, target_dir):
         with gzip.open(filepath, 'rb') as gzipf:
             with open(filepath[:-3], 'wb') as outf:
                 outf.write(gzipf.read())
+    elif '.7z' in filepath:
+        if os.name != 'posix':
+            raise NotImplementedError('Only Linux and Mac OS X support .7z '
+                                      'compression.')
+        print('Using 7z!!!')
+        cmd = '7z x {} -o{}'.format(filepath, target_dir)
+        retval = Popen(cmd, shell=True).wait()
+        if retval != 0:
+            raise RuntimeError(
+                'Archive file extraction failed for {}.'.format(filepath))
     elif filepath[-2:].lower() == '.z':
         if os.name != 'posix':
             raise NotImplementedError('Only Linux and Mac OS X support .Z '
                                       'compression.')
-        cmd = 'gzip -d %s' % filepath
+        cmd = 'gzip -d {}'.format(filepath)
         retval = Popen(cmd, shell=True).wait()
         if retval != 0:
             raise RuntimeError(
-                'Archive file extraction failed for %s.' % filepath)
+                'Archive file extraction failed for {}.'.format(filepath))
     else:
-        raise ValueError('% is not a supported archive file.' % filepath)
+        raise ValueError('{} is not a supported archive file.'.format(filepath))
 
 
-def download_celabA(datasets_dir='datasets'):
+def download_celabA(datasets_dir='celeba'):
+    # dataset_dir='celeba'
 
     _IMGS_URL = ('https://www.dropbox.com/sh/8oqt9vytwxb3s4r/AAAq9krDJxUMh1m0hbbxdnl4a/Img/img_celeba.7z?dl=1',
-        '')
+        '37af560349c7d2e51fcc4461168452c743c9cb96')
 
     _ALIGNED_IMGS_URL = (
         'https://www.dropbox.com/sh/8oqt9vytwxb3s4r/AADIKlz8PR9zr6Y20qbkunrba/Img/img_align_celeba.zip?dl=1',
@@ -86,94 +96,83 @@ def download_celabA(datasets_dir='datasets'):
         'https://www.dropbox.com/sh/8oqt9vytwxb3s4r/AADxLE5t6HqyD8sQCmzWJRcHa/Eval/list_eval_partition.txt?dl=1',
         'fb3d89825c49a2d389601eacb10d73815fd3c52d')
 
-    _ATTRIBUTES_URL = (
+    _ALIGNED_ATTRIBUTES_URL = (
         'https://www.dropbox.com/sh/8oqt9vytwxb3s4r/AAC7-uCaJkmPmvLX2_P5qy0ga/Anno/list_attr_celeba.txt?dl=1',
         '225788ff6c9d0b96dc21144147456e0388195617')
 
-    name = 'celeba'
+    _ATTRIBUTES_URL = (
+        'https://www.dropbox.com/sh/8oqt9vytwxb3s4r/AAANSiC4E2KoizMw92lvJTOta/Anno/list_landmarks_celeba.txt?dl=1',
+        'ea255cd0ffe98ca88bff23767f7a5ece7710db57')
+
     n_imgs = 202599
-    data_dir = os.path.join(datasets_dir, name)
-    npz_path = os.path.join(data_dir, name + '.npz')
-    img_dir = os.path.join(data_dir, 'img_align_celeba')
-
-    url, sha1 = _IMGS_URL
-    print('Downloading {}'.format(url))
-    filepath = download(url, datasets_dir)
-    print('Done!')
-    print('Check SHA1 {}'.format(filepath))
-    print(checksum(filepath, 'sha1'))
-    # if sha1 != checksum(filepath, 'sha1'):
-    #     raise RuntimeError('Checksum mismatch for %s.' % url)
-
-    print('Extract archive {}'.format(filepath))
-    archive_extract(filepath, data_dir)
-    print('Done!')
-    os.remove(filepath)
-
+    img_dir_align = os.path.join(dataset_dir, 'Img', 'img_align_celeba')
+    img_dir = os.path.join(dataset_dir, 'Img', 'img_celeba')
 
     url, sha1 = _ALIGNED_IMGS_URL
     print('Downloading {}'.format(url))
-    filepath = download(url, datasets_dir)
+    filepath = download(url, dataset_dir)
     print('Done!')
     print('Check SHA1 {}'.format(filepath))
     if sha1 != checksum(filepath, 'sha1'):
         raise RuntimeError('Checksum mismatch for %s.' % url)
 
     print('Extract archive {}'.format(filepath))
-    archive_extract(filepath, data_dir)
+    archive_extract(filepath, os.path.join(dataset_dir, 'Img'))
     print('Done!')
     os.remove(filepath)
 
+    n_imgsd = sum([1 for file in os.listdir(img_dir_align) if file[-4:] == '.jpg'])
+    assert (n_imgsd == n_imgs)
+
     url, sha1 = _PARTITIONS_URL
     print('Downloading {}'.format(url))
-    filepath = download(url, datasets_dir)
+    filepath = download(url, os.path.join(dataset_dir,'Eval'))
     print('Done!')
     print('Check SHA1 {}'.format(filepath))
     if sha1 != checksum(filepath, 'sha1'):
         raise RuntimeError('Checksum mismatch for %s.' % url)
-    partitions = [[], [], []]
-    with open(filepath, 'r') as f:
-        for i, line in enumerate(f):
-            img_name, partition = line.strip().split(' ')
-            if int(img_name[:6]) != i + 1:
-                raise ValueError('Parse error.')
-            partition = int(partition)
-            partitions[partition].append(i)
-    train_idxs, val_idxs, test_idxs = map(np.array, partitions)
-    # os.remove(filepath)
-    n_imgsd = sum([1 for file in os.listdir(img_dir) if file[-4:] == '.jpg'])
-    assert (n_imgsd == n_imgs)
 
     url, sha1 = _ATTRIBUTES_URL
     print('Downloading {}'.format(url))
-    filepath = download(url, datasets_dir)
+    filepath = download(url, os.path.join(dataset_dir,'Anno'))
     print('Done!')
     print('Check SHA1 {}'.format(filepath))
     if sha1 != checksum(filepath, 'sha1'):
         raise RuntimeError('Checksum mismatch for %s.' % url)
-    attributes = []
-    with open(filepath, 'r') as f:
-        f.readline()
-        attribute_names = f.readline().strip().split(' ')
-        for i, line in enumerate(f):
-            fields = line.strip().replace('  ', ' ').split(' ')
-            img_name = fields[0]
-            if int(img_name[:6]) != i + 1:
-                raise ValueError('Parse error.')
-            attr_vec = np.array(map(int, fields[1:]))
-            attributes.append(attr_vec)
-    attributes = np.array(attributes)
-    # os.remove(filepath)
 
-    with open(npz_path, 'wb') as f:
-        np.savez(
-            f,
-            train_idxs=train_idxs,
-            val_idxs=val_idxs,
-            test_idxs=test_idxs,
-            attribute_names=attribute_names,
-            attributes=attributes)
-    return True
+    url, sha1 = _ALIGNED_ATTRIBUTES_URL
+    print('Downloading {}'.format(url))
+    filepath = download(url, os.path.join(dataset_dir,'Anno'))
+    print('Done!')
+    print('Check SHA1 {}'.format(filepath))
+    if sha1 != checksum(filepath, 'sha1'):
+        raise RuntimeError('Checksum mismatch for %s.' % url)
+
+    url, sha1 = _IMGS_URL
+    print('Downloading {}'.format(url))
+    filepath = download(url, dataset_dir)
+    print('Done!')
+    print('Check SHA1 {}'.format(filepath))
+    if sha1 != checksum(filepath, 'sha1'):
+        raise RuntimeError('Checksum mismatch for %s.' % url)
+
+    print('Extract archive {}'.format(filepath))
+    archive_extract(filepath, dataset_dir)
+    os.remove(filepath)
+    filepath = os.path.join(dataset_dir, 'img_celeba.7z.001')
+    archive_extract(filepath, os.path.join(dataset_dir, 'Img'))
+    print('Done!')
+
+    for file in os.listdir(dataset_dir):
+        if file[:14] == 'img_celeba.7z.':
+            filepath = os.path.join(dataset_dir, file)
+            print('Remove: {}'.format(filepath))
+            os.remove(filepath)
+
+    n_imgsd = sum([1 for file in os.listdir(img_dir) if file[-4:] == '.jpg'])
+    assert (n_imgsd == n_imgs)
+
+return True
 
 if __name__ == '__main__':
     download_celabA()
