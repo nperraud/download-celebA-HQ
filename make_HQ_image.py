7adfc7c00888cd1f5e5ea92b502d2c2d594e4d4d
Nathanael Perraudin <nperraud@users.noreply.github.com>
2018-04-24 16:17:07 +0200

    add pool

--- make_HQ_image.py
+++ make_HQ_image.py
@@ -12,91 +12,6 @@ import base64
 import bz2
 import scipy
 from scipy import ndimage
-import threading
-import traceback
-import sys
-import six.moves.queue as Queue
-
-class ExceptionInfo(object):
-    def __init__(self):
-        self.value = sys.exc_info()[1]
-        self.traceback = traceback.format_exc()
-
-
-class WorkerThread(threading.Thread):
-    def __init__(self, task_queue):
-        threading.Thread.__init__(self)
-        self.task_queue = task_queue
-
-    def run(self):
-        while True:
-            func, args, result_queue = self.task_queue.get()
-            if func is None:
-                break
-            try:
-                result = func(*args)
-            except:
-                result = ExceptionInfo()
-            result_queue.put((result, args))
-
-class ThreadPool(object):
-    def __init__(self, num_threads):
-        assert num_threads >= 1
-        self.task_queue = Queue.Queue()
-        self.result_queues = dict()
-        self.num_threads = num_threads
-        for idx in range(self.num_threads):
-            thread = WorkerThread(self.task_queue)
-            thread.daemon = True
-            thread.start()
-
-    def add_task(self, func, args=()):
-        assert hasattr(func, '__call__') # must be a function
-        if func not in self.result_queues:
-            self.result_queues[func] = Queue.Queue()
-        self.task_queue.put((func, args, self.result_queues[func]))
-
-    def get_result(self, func): # returns (result, args)
-        result, args = self.result_queues[func].get()
-        if isinstance(result, ExceptionInfo):
-            print('\n\nWorker thread caught an exception:\n' + result.traceback)
-            raise result.value
-        return result, args
-
-    def finish(self):
-        for idx in range(self.num_threads):
-            self.task_queue.put((None, (), None))
-
-    def __enter__(self): # for 'with' statement
-        return self
-
-    def __exit__(self, *excinfo):
-        self.finish()
-
-    def process_items_concurrently(self, item_iterator, process_func=lambda x: x, pre_func=lambda x: x, post_func=lambda x: x, max_items_in_flight=None):
-        if max_items_in_flight is None: max_items_in_flight = self.num_threads * 4
-        assert max_items_in_flight >= 1
-        results = []
-        retire_idx = [0]
-
-        def task_func(prepared, idx):
-            return process_func(prepared)
-           
-        def retire_result():
-            processed, (prepared, idx) = self.get_result(task_func)
-            results[idx] = processed
-            while retire_idx[0] < len(results) and results[retire_idx[0]] is not None:
-                yield post_func(results[retire_idx[0]])
-                results[retire_idx[0]] = None
-                retire_idx[0] += 1  
-        for idx, item in enumerate(item_iterator):
-            prepared = pre_func(item)
-            results.append(None)
-            self.add_task(func=task_func, args=(prepared, idx))
-            while retire_idx[0] < idx - max_items_in_flight + 2:
-                for res in retire_result(): yield res
-        while retire_idx[0] < len(results):
-            for res in retire_result(): yield res
 
 
 dataset_dir='celeba'
@@ -230,14 +145,12 @@ def process_func(idx):
     assert md5.hexdigest() == fields['final_md5'][idx]
     return img
 
-num_threads=num_tasks
-with ThreadPool(num_threads) as pool:
-    for img_num, img in enumerate(pool.process_items_concurrently(indices.tolist(), process_func=process_func, max_items_in_flight=num_tasks)):
-        print('Create image number {}'.format(img_num))
-        img = process_func(img_num)
-        np.save(os.path.join(delta_dir, 'imgHQ%05d' % img_num), [img])
 
-# for img_num in range(expected_dat):
-#     print('Create image number {}'.format(img_num))
-#     img = process_func(img_num)
-#     np.save(os.path.join(delta_dir, 'imgHQ%05d' % img_num), [img])
+def do_the_work(img_num):
+    print('Create image number {}'.format(img_num))
+    img = process_func(img_num)
+    np.save(os.path.join(delta_dir, 'imgHQ%05d' % img_num), [img])
+
+for img_num in range(expected_dat):
+    do_the_work(img_num)
+
